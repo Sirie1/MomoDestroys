23e1aa6e0ea18bde76fa6d6c5e07b49aaad2db35
Sebastian Irie <sebairie@gmail.com>
2023-04-29 18:22:13 +0100

    Furniture animations

--- Assets/Scripts/Furniture.cs
+++ Assets/Scripts/Furniture.cs
@@ -2,16 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class Furniture : MonoBehaviour
 {
    // [SerializeField] bool mouthContact = false;
     [Range(0.0f, 100.0f)] [SerializeField] private float health = 100f;
+    [SerializeField] float shakeIntensity = 0.05f;
+    //[SerializeField] float shakeSpeed = 5f;
+    [SerializeField] bool isShakeOn;
+    [SerializeField] bool isShakeAnimOn;
     [SerializeField] Slider slider;
     [SerializeField] SpriteRenderer brokenSpriteRenderer;
+    Vector2 originalPosition;
     private void Start()
     {
         RestoreHealth();
+        isShakeOn = false;
+        originalPosition = transform.localPosition;
+    }
+    private void Update()
+    {
+        //Shake
+        if(isShakeOn)
+        {
+            if (!isShakeAnimOn)
+            {
+                isShakeAnimOn = true;
+                transform.DOShakePosition(0.1f, shakeIntensity).onComplete = ResetPosition;
+            }
+        }
+        isShakeOn=false;
+
+    }
+    private void ResetPosition ()
+    {
+        isShakeAnimOn = false;
+        transform.localPosition = originalPosition;
 
     }
     private void RestoreHealth()
@@ -25,11 +52,16 @@ public class Furniture : MonoBehaviour
     {
         if (health>0)
         {
+            StartShake();
             health -= chewPower * Time.deltaTime;
             ScoreManager.Instance.AddScore();
             slider.value = health / 100;
             UpdateSprite();
         }
+    }
+    public void StartShake()
+    {
+        isShakeOn = true;
     }
     void UpdateSprite()
     {

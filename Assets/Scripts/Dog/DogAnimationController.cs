8ed422b8d098dd774db15e1a731266eae6b98b1f
Sebastian Irie <sebairie@gmail.com>
2023-05-27 16:08:08 +0100

    Set Tofu animations

--- Assets/Scripts/Dog/DogAnimationController.cs
+++ Assets/Scripts/Dog/DogAnimationController.cs
@@ -1,6 +1,8 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 //using static DogStateController;
 
 public class DogAnimationController : MonoBehaviour
@@ -9,14 +11,26 @@ public class DogAnimationController : MonoBehaviour
     public Animator animator;
     [SerializeField] DogPUController dogPUController;
     [SerializeField] DogStateController dogStateController;
+    [SerializeField] string currentSkin;
     // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        
+        SetMomo();
     }
     public void SetAnimation()
     {
         //Debug.Log (dogPUController.CurrentPowerUp.ToString() + "_" + dogStateController.CurrentStateName.ToString());
-        animator.Play(dogPUController.CurrentPowerUp.ToString() + "_" + dogStateController.CurrentStateName.ToString());
+        animator.Play(currentSkin + dogPUController.CurrentPowerUp.ToString() + "_" + dogStateController.CurrentStateName.ToString());
+    }
+    public void SetMomo()
+    {
+        animator.StopPlayback();
+        currentSkin = "Momo_";
+    }
+    public void SetTofu()
+    {
+        animator.StopPlayback();
+        currentSkin = "Tofu_";
     }
 }
